8611514699512ba2d78036e29c3d458a08642856
Aditya K G <akarnam37@gmail.com>
2015-02-18 23:07:52 +0530

    Trending News fetch from Server

--- app/src/main/java/in/niooz/niooz/HomeActivity.java
+++ app/src/main/java/in/niooz/niooz/HomeActivity.java
@@ -1,7 +1,9 @@
 package in.niooz.niooz;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -13,11 +15,17 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class HomeActivity extends ActionBarActivity {
 
     private SwipeRefreshLayout mSwipeRefreshLayout;
-    TextView hl1,hl2,hl3,hl4;
+    private TextView hl1,hl2,hl3,hl4;
+    private static String TRENDING_URL = "http://itechnospot.com/temp/trending.php";
+    private ProgressDialog pDialog1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +49,8 @@ public class HomeActivity extends ActionBarActivity {
             public void onRefresh() {
                 mSwipeRefreshLayout.setRefreshing(true);
                 Log.d("Swipe", "Refreshing Number");
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        mSwipeRefreshLayout.setRefreshing(false);
-                    }
-                }, 2500);
+                new LoadTrendingNews().execute();
+                mSwipeRefreshLayout.setRefreshing(false);
             }
         });
 
@@ -56,6 +60,55 @@ public class HomeActivity extends ActionBarActivity {
         hl2 = (TextView) findViewById(R.id.trendingHead2);
         hl3 = (TextView) findViewById(R.id.trendingHead3);
         hl4 = (TextView) findViewById(R.id.trendingHead4);
+
+
+
+        new LoadTrendingNews().execute();
+    }
+
+    public class LoadTrendingNews extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            ServiceHandler sh = new ServiceHandler();
+            final String respStr = sh.makeServiceCall(TRENDING_URL, ServiceHandler.POST);
+            try {
+
+                //Log.d("Response: ", "> " + res);
+                JSONObject jsonObject = new JSONObject(respStr);
+                final String th1 = jsonObject.getString("t1");
+                final String th2 = jsonObject.getString("t2");
+                final String th3 = jsonObject.getString("t3");
+                final String th4 = jsonObject.getString("t4");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        hl1.setText(th1);
+                        hl2.setText(th2);
+                        hl3.setText(th3);
+                        hl4.setText(th4);
+
+                    }
+                });
+
+            }catch (JSONException ex){
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+        }
+
 
     }
 
